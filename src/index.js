e4b079ec2aab2f6a495d1f5d1a76059516b41d36
ssekridov@gmail.com <ssekridov@gmail.com>
2022-08-04 00:15:52 +0300

    feat task solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function toReadable (number) {
-  
+    const ones = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+    const tens = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+
+    if (number < 20) {
+        return `${ones[number]}`
+    }
+    if (number < 100) {
+        return `${tens[~~((number / 10) - 2)]}${(number % 10 ? ` ${ones[~~(number % 10)]}` : '')}`
+    }
+    if (number < 1000) {
+        return `${ones[~~(number / 100)]} hundred${(number % 100 === 0 ? '' : ` ${toReadable(number % 100)}`)}`
+    }
 }
